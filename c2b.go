bc9d187c0a73a4f0b9e7adb2e75cb302f30eb385
Victor Ndaba <mwangindaba@gmail.com>
2022-09-03 22:52:39 +0300

    feat: created the c2b builder

--- c2b.go
+++ c2b.go
@@ -1,3 +1,11 @@
 package daraja
 
-type customerToBusiness struct{}
+type customerToBusiness struct {
+	_appRef *mpesa
+}
+
+func c2bBuilder(app *mpesa) *customerToBusiness {
+	return &customerToBusiness{
+		_appRef: app,
+	}
+}
