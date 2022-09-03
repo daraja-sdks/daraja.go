bc20fdf429d6fafc52390f9cc81b5e3f7a26b410
Victor Ndaba <mwangindaba@gmail.com>
2022-09-03 22:38:45 +0300

    feat: started work on the b2c builder

--- b2c.go
+++ b2c.go
@@ -1,3 +1,11 @@
 package daraja
 
-type businessToCustomer struct{}
+type businessToCustomer struct {
+	_appRef *mpesa
+}
+
+func b2cBuilder(app *mpesa) *businessToCustomer {
+	return &businessToCustomer{
+		_appRef: app,
+	}
+}
