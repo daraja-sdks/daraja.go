4263174ce1a5b5a23d422f2b37563514701e6cea
Victor Ndaba <mwangindaba@gmail.com>
2022-09-03 22:50:40 +0300

    feat: added b2c builder methods

--- b2c.go
+++ b2c.go
@@ -1,11 +1,35 @@
 package daraja
 
 type businessToCustomer struct {
-	_appRef *mpesa
+	_appRef      *mpesa
+	_Initiator   string
+	_ShortCode   int32
+	_PhoneNumber int32
+	_CommandID   string
+	_Remarks     string
+	_TimeoutURL  string
+	_ResultURL   string
+	_Occassion   string
 }
 
 func b2cBuilder(app *mpesa) *businessToCustomer {
 	return &businessToCustomer{
-		_appRef: app,
+		_appRef:    app,
+		_CommandID: "CustomerPayBillOnline",
 	}
+}
+
+func (b *businessToCustomer) ShortCode(code int32) *businessToCustomer {
+	b._ShortCode = code
+	return b
+}
+
+func (b *businessToCustomer) PhoneNumber(value int32) *businessToCustomer {
+	b._PhoneNumber = value
+	return b
+}
+
+func (b *businessToCustomer) ResultURL(url string) *businessToCustomer {
+	b._ResultURL = url
+	return b
 }
